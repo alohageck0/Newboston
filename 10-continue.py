80bde5241d49c55fea7e6836b5f884ba1e72c657
alohageck0 <royalfiish@gmail.com>
2015-07-05 21:40:47 -0700

    update

--- 10-continue.py
+++ 10-continue.py
@@ -1,7 +1,8 @@
 __author__ = 'royalfiish'
 numbersTaken = [4, 6, 7, 11, 15]
 
-for n in numbersTaken:
+for n in range(30):
     if n in numbersTaken:
+        print("---\n")
         continue
     print(n)
