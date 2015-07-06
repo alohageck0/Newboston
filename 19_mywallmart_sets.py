d31786cab8beba694ca2f41daf75ca93a37a85d2
alohageck0 <royalfiish@gmail.com>
2015-07-06 14:54:13 -0700

    init

---
+++ 19_mywallmart_sets.py
@@ -0,0 +1,9 @@
+__author__ = 'royalfiish'
+
+shopping_list = {'milk', 'bread', 'eggs', 'milk', 'beer', 'apple', 'eggs'}
+
+print(shopping_list)
+if 'eggs' not in shopping_list:
+    print("yes")
+elif 'lll' not in shopping_list:
+    print("no")
