ca680396e6cadb81c9ecdce1d0b1903e52473da0
alohageck0 <royalfiish@gmail.com>
2015-07-05 21:14:35 -0700

    my version-break

--- break-comment.py
+++ break-comment.py
@@ -2,7 +2,8 @@ __author__ = 'royalfiish'
 magicNumber = 66
 step = 1
 for magicNumber in range(100):
-    if magicNumber = 66:
+    if magicNumber == 66:
         print("Yes")
+        break
     step +=1
 print(step)
