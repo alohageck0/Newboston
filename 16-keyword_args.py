af2129ebafa5d4288f136767972c06c0393257d7
alohageck0 <royalfiish@gmail.com>
2015-07-06 14:26:41 -0700

    init 1

--- 16-keyword_args.py
+++ 16-keyword_args.py
@@ -1,3 +1,8 @@
 __author__ = 'royalfiish'
 
 def foo(name='Boston', action = 'eat', item = 'apple'):
+    print(name, action, item)
+
+# foo()
+foo('Jack', 'drinks', 'soda')
+foo(action='drinks', name='John')
