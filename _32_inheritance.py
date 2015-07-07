6a1eb82c511d5f3a7ec39205f284bb7acc13c5a7
alohageck0 <royalfiish@gmail.com>
2015-07-07 16:59:33 -0700

    init

---
+++ _32_inheritance.py
@@ -0,0 +1,24 @@
+__author__ = 'royalfiish'
+
+class Parent():
+
+    def print_last_name(self):
+        print("Last_Name")
+
+class Child(Parent):
+
+    def print_first_name(self):
+        print("First_name")
+
+    def print_last_name(self):
+        print("New_last_name")
+
+child = Child()
+parent = Parent()
+
+child.print_first_name()
+child.print_last_name()
+
+parent.print_last_name()
+
+
