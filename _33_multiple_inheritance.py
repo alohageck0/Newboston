4a7b5431a6c863c7f6306ae0904cf7d7bc6f3f94
alohageck0 <royalfiish@gmail.com>
2015-07-07 20:37:20 -0700

    update

--- _33_multiple_inheritance.py
+++ _33_multiple_inheritance.py
@@ -1,25 +1,25 @@
 __author__ = 'royalfiish'
 
-class Parent():
 
+class Parent():
     def print_last_name(self):
         print("Last_Name")
 
-class Sibling():
 
+class Sibling():
     def toy(self):
         print("I have toy")
 
-class Child(Parent, Sibling):
 
+class Child(Parent, Sibling):
     def print_first_name(self):
         print("First_name")
 
     def print_last_name(self):
         print("New_last_name")
 
-child = Child()
 
+child = Child()
 
 child.print_first_name()
 child.print_last_name()
