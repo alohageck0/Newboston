4e3c0c95c1760a0ca71fb9128f3bf8abb0437141
alohageck0 <royalfiish@gmail.com>
2015-07-07 17:35:39 -0700

    update

--- _34_threading.py
+++ _34_threading.py
@@ -1,11 +1,15 @@
 __author__ = 'royalfiish'
 
 import threading
+import time
 
 class Messenger(threading.Thread):
     def run(self):
         for _ in range(10):
             print(threading.current_thread().getName())
+            time.sleep(0.5)
 
-x = Messenger()
-y = Messenger()
+x = Messenger(name='First thread')
+y = Messenger(name='Second thread')
+x.start()
+y.start()
