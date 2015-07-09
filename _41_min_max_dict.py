32f13683df42d58cdfb04353f2c955682597f887
alohageck0 <royalfiish@gmail.com>
2015-07-09 15:24:34 -0700

    init

--- _41_min_max_dict.py
+++ _41_min_max_dict.py
@@ -1,3 +1,8 @@
 __author__ = 'royalfiish'
 
 stocks = {'GOOG': 444, 'AAPL': 333, 'AMZN': 143, 'SONY': 532}
+
+print(min(zip(stocks.values(), stocks.keys())))
+print(max(zip(stocks.values(), stocks.keys())))
+print(sorted(zip(stocks.values(), stocks.keys())))
+print(sorted(zip(stocks.keys(), stocks.values())))
