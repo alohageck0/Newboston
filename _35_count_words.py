d49cfdad4ce78936cb99589dfde77ead1222f224
alohageck0 <royalfiish@gmail.com>
2015-07-08 20:49:20 -0700

    update

--- _35_count_words.py
+++ _35_count_words.py
@@ -4,9 +4,17 @@ import requests
 from bs4 import BeautifulSoup
 import operator
 
+
 def start(url):
     word_list = []
     source = requests.get(url).text
-    soup = BeautifulSoup(source)
+    soup = BeautifulSoup(source, "html.parser")
+    for text in soup.find_all('a'):
+        content = str(text.string)
+        words = content.lower().split()
+        for word in words:
+            word_list.append(word)
+            # print(word)
 
-# http://www.govorimpro.us/
+
+start('http://www.govorimpro.us/')
