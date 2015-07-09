8dc4a1940d113e72917d75dd9f708b319a44ad98
alohageck0 <royalfiish@gmail.com>
2015-07-08 21:34:29 -0700

    update 2

--- _35_count_words.py
+++ _35_count_words.py
@@ -15,6 +15,29 @@ def start(url):
         for word in words:
             word_list.append(word)
             # print(word)
+    clean_list(word_list)
 
 
-start('http://www.govorimpro.us/')
+def clean_list(word_list):
+    clean_list = []
+    for word in word_list:
+        symbols = "!!!@#$%^&*()_+<>?\"{}|,:./"
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], "")
+        if len(word) > 0:
+            clean_list.append(word)
+    count_words(clean_list)
+
+
+def count_words(list):
+    counted = dict()
+    for word in list:
+        if word in counted:
+            counted[word] += 1
+        else:
+            counted[word] = 1
+    for k, v in sorted(counted.items(), key=operator.itemgetter(1), reverse=1):
+        print(k, '-', v)
+
+
+start('http://www.finance.yahoo.com')
